3d95c0a27447ec63371f23499c701fff24eee453
Maksim Shalaev <maksimshalaev11@gmail.com>
2023-07-09 22:54:18 +0300

    Added tests

---
+++ src/test/java/ru/netology/service/CashBackHackerServiceTest.java
@@ -0,0 +1,41 @@
+package ru.netology.service;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.Assert;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashBackHackerServiceTest{
+
+    @org.junit.Test
+    public void shouldCalculateRemaind900() {
+        CashBackHackerService service = new CashBackHackerService();
+        int amount = 100;
+        int expected = 900;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void shouldCalculateRemaind999() {
+        CashBackHackerService service = new CashBackHackerService();
+        int amount = 1;
+        int expected = 999;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void shouldCalculateRemaind0() {
+        CashBackHackerService service = new CashBackHackerService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+
+
+
+}
