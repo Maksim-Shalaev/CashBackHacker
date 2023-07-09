d52f1d16892ee2d90942ea7b7c85dfb733738b5c
Maksim Shalaev <maksimshalaev11@gmail.com>
2023-07-09 20:38:52 +0300

    Added tests

---
+++ src/test/java/ru/netology/service/CashBackHackerServiceTest.java
@@ -0,0 +1,40 @@
+package ru.netology.service;
+
+class CashBackHackerServiceTest {
+
+    @org.testng.annotations.Test
+    void shouldCalculateRemainder500() {
+        CashBackHackerService service = new CashBackHackerService();
+        int amount = 500;
+        int expected = 500;
+        int actual = service.remain(amount);
+        org.testng.Assert.assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    void shouldCalculateRemainder900() {
+        CashBackHackerService service = new CashBackHackerService();
+        int amount = 900;
+        int expected = 100;
+        int actual = service.remain(amount);
+        org.testng.Assert.assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    void shouldCalculateRemainder0() {
+        CashBackHackerService service = new CashBackHackerService();
+        int amount = 0;
+        int expected = 1000;
+        int actual = service.remain(amount);
+        org.testng.Assert.assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    void shouldCalculateRemainder1000() {
+        CashBackHackerService service = new CashBackHackerService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        org.testng.Assert.assertEquals(actual, expected);
+    }
+}
